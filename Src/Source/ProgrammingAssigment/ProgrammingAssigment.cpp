2844e42ae878ed44bd3d0c50a92fe928f949b6e4
twinkocat <msbulahmaxim@gmail.com>
2025-06-08 18:45:54 +0300

    [INIT] initial commit

---
+++ Src/Source/ProgrammingAssigment/ProgrammingAssigment.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProgrammingAssigment.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProgrammingAssigment, "ProgrammingAssigment" );
