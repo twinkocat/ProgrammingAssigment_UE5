da3a7f9176f71887251ecf1a896cde016fcb9918
twinkocat <msbulahmaxim@gmail.com>
2025-06-12 13:00:36 +0300

    [ADD] Session feature

---
+++ Src/Source/ProgrammingAssigment/Private/BlueprintLibraries/MyBlueprintHelperLibrary.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlueprintLibraries/MyBlueprintHelperLibrary.h"
+#include "MyGameInstance.h"
+
+UMyGameInstance* UMyBlueprintHelperLibrary::GetMyGameInstance(const UObject* Context)
+{
+	const UWorld* World = GEngine->GetWorldFromContextObjectChecked(Context);
+	return World->GetGameInstance<UMyGameInstance>(); 
+}
---
+++ Src/Source/ProgrammingAssigment/Public/BlueprintLibraries/MyBlueprintHelperLibrary.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyBlueprintHelperLibrary.generated.h"
+
+class UMyGameInstance;
+/**
+ * 
+ */
+UCLASS()
+class PROGRAMMINGASSIGMENT_API UMyBlueprintHelperLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "MyBlueprintHelperLibrary", meta = (WorldContext = "Context"))
+	static UMyGameInstance* GetMyGameInstance(const UObject* Context);
+};
